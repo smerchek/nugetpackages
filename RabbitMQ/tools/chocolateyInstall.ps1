e537d1934944bd05df4ff1c06290ff4bab8e4a95
Kevin Sheehan <kms1053email-10@yahoo.com>
2013-01-28 11:40:27 -0500

    Updated for RabbitMQ 3.0.1 and Erlang 15.3.1

--- RabbitMQ/tools/chocolateyInstall.ps1
+++ RabbitMQ/tools/chocolateyInstall.ps1
@@ -1,1 +1,16 @@
-﻿Install-ChocolateyPackage 'rabbitmq' 'EXE' '/S' 'http://www.rabbitmq.com/releases/rabbitmq-server/v2.8.2/rabbitmq-server-2.8.2.exe' -validExitCodes @(0)
+﻿
+try {
+
+	Install-ChocolateyPackage 'rabbitmq' 'EXE' '/S' 'http://www.rabbitmq.com/releases/rabbitmq-server/v3.0.1/rabbitmq-server-3.0.1.exe' -validExitCodes @(0)
+
+	Start-ChocolateyProcessAsAdmin 'C:\Chocolatey\lib\rabbitmq.3.0.1\tools\SetupRabbitMqManagement.bat'
+				    
+  echo ""
+  echo "RabbitMQ Management Plugin enabled by default at http://localhost:15672"
+  echo ""
+
+  Write-ChocolateySuccess 'RabbitMQ'
+} catch {
+  Write-ChocolateyFailure 'RabbitMQ' $($_.Exception.Message)
+  throw 
+}
