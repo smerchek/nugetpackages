0ea042036f2c167b2397201e61feda39730fdc68
vorou <voropaev.viktor@gmail.com>
2013-08-16 19:31:26 +0600

    Fixed RabbitMQ path for 3.1.5

--- RabbitMQ/tools/SetupRabbitMqManagement.bat
+++ RabbitMQ/tools/SetupRabbitMqManagement.bat
@@ -1,4 +1,4 @@
-call "C:\Program Files (x86)\RabbitMQ Server\rabbitmq_server-3.0.1\sbin\rabbitmq-service.bat" stop
-call "C:\Program Files (x86)\RabbitMQ Server\rabbitmq_server-3.0.1\sbin\rabbitmq-plugins.bat" enable rabbitmq_management
-call "C:\Program Files (x86)\RabbitMQ Server\rabbitmq_server-3.0.1\sbin\rabbitmq-service.bat" install
-call "C:\Program Files (x86)\RabbitMQ Server\rabbitmq_server-3.0.1\sbin\rabbitmq-service.bat" start
+call "C:\Program Files (x86)\RabbitMQ Server\rabbitmq_server-3.1.5\sbin\rabbitmq-service.bat" stop
+call "C:\Program Files (x86)\RabbitMQ Server\rabbitmq_server-3.1.5\sbin\rabbitmq-plugins.bat" enable rabbitmq_management
+call "C:\Program Files (x86)\RabbitMQ Server\rabbitmq_server-3.1.5\sbin\rabbitmq-service.bat" install
+call "C:\Program Files (x86)\RabbitMQ Server\rabbitmq_server-3.1.5\sbin\rabbitmq-service.bat" start
