9fa5b559d3d2f66646e4111779a03cf42661390f
atagirov <tagirovarthur@gmail.com>
2014-11-10 08:37:27 +0500

    Update chocolateyInstall.ps1
    Chocolate from 0.9.8.24 version use "C:\ProgramData\chocolatey" folder for install instead "C:\Chocolatey". For ignoring this changes in future, we are locate SetupRabbitMqManagement.bat using path to chocolateyInstall.ps1

--- RabbitMQ/tools/chocolateyInstall.ps1
+++ RabbitMQ/tools/chocolateyInstall.ps1
@@ -3,7 +3,10 @@ try {
 
 	Install-ChocolateyPackage 'rabbitmq' 'EXE' '/S' 'http://www.rabbitmq.com/releases/rabbitmq-server/v3.4.1/rabbitmq-server-3.4.1.exe' -validExitCodes @(0)
 
-	Start-ChocolateyProcessAsAdmin 'C:\Chocolatey\lib\rabbitmq.3.4.1.0\tools\SetupRabbitMqManagement.bat'
+	$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+	$target = Join-Path $scriptPath "SetupRabbitMqManagement.bat"
+
+	Start-ChocolateyProcessAsAdmin $target
 				    
   echo ""
   echo "RabbitMQ Management Plugin enabled by default at http://localhost:15672"
