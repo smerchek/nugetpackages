726ba6320f50d3752254133e0b470ef4e3226eff
Robert Labrie <robert.labrie@gmail.com>
2015-04-22 10:05:19 -0400

    new version and silence a warn

--- RabbitMQ/tools/SetupRabbitMqManagement.bat
+++ RabbitMQ/tools/SetupRabbitMqManagement.bat
@@ -1,4 +1,4 @@
-call "C:\Program Files (x86)\RabbitMQ Server\rabbitmq_server-3.4.1\sbin\rabbitmq-service.bat" stop
-call "C:\Program Files (x86)\RabbitMQ Server\rabbitmq_server-3.4.1\sbin\rabbitmq-plugins.bat" enable rabbitmq_management
-call "C:\Program Files (x86)\RabbitMQ Server\rabbitmq_server-3.4.1\sbin\rabbitmq-service.bat" install
-call "C:\Program Files (x86)\RabbitMQ Server\rabbitmq_server-3.4.1\sbin\rabbitmq-service.bat" start
+call "C:\Program Files (x86)\RabbitMQ Server\rabbitmq_server-3.5.1\sbin\rabbitmq-service.bat" stop
+call "C:\Program Files (x86)\RabbitMQ Server\rabbitmq_server-3.5.1\sbin\rabbitmq-plugins.bat" enable rabbitmq_management --offline
+call "C:\Program Files (x86)\RabbitMQ Server\rabbitmq_server-3.5.1\sbin\rabbitmq-service.bat" install
+call "C:\Program Files (x86)\RabbitMQ Server\rabbitmq_server-3.5.1\sbin\rabbitmq-service.bat" start
