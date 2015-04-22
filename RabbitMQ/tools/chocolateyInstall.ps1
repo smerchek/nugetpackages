48106cae5c522a159d7b03a34856499f096268e0
Robert Labrie <robert.labrie@gmail.com>
2015-04-22 10:05:27 -0400

    new version

--- RabbitMQ/tools/chocolateyInstall.ps1
+++ RabbitMQ/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿
 try {
 
-	Install-ChocolateyPackage 'rabbitmq' 'EXE' '/S' 'http://www.rabbitmq.com/releases/rabbitmq-server/v3.4.1/rabbitmq-server-3.4.1.exe' -validExitCodes @(0)
+	Install-ChocolateyPackage 'rabbitmq' 'EXE' '/S' 'http://www.rabbitmq.com/releases/rabbitmq-server/v3.5.1/rabbitmq-server-3.5.1.exe' -validExitCodes @(0)
 
 	$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 	$target = Join-Path $scriptPath "SetupRabbitMqManagement.bat"
